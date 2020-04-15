1e9cae1cb03cce2a2d92bd87cfbb67f8f994f9a4
’xiangbin.huang‘ <xiangbin.huang@ubtrobot.com>
2020-04-15 15:50:47 +0800

    baiduMap

---
+++ baiduMap/Map/mainwindow.cpp
@@ -0,0 +1,30 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QWebChannel>
+#include <QUrl>
+#include <QWebEngineHistory>
+#include <QWebEnginePage>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QString path(qApp->applicationDirPath() + "/index.html");
+    qDebug() << path;
+    ui->webView->load(QUrl(path));
+    ui->webView->show();
+
+    connect(&m_timer, SIGNAL(timeout()), this, SLOT(onTimeOut()));
+    m_timer.start(1000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onTimeOut()
+{
+
+}
---
+++ baiduMap/Map/mainwindow.h
@@ -0,0 +1,30 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWebEngineView>
+#include <QTimer>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void onTimeOut();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer m_timer;
+
+//    QWebEngineView* webView;
+};
+
+#endif // MAINWINDOW_H
