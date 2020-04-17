60c093290ba0e46ea1a591341b419ed7b6060d7a
’xiangbin.huang‘ <xiangbin.huang@ubtrobot.com>
2020-04-17 19:25:49 +0800

    jna

---
+++ jna-c-master/jna.h
@@ -0,0 +1,13 @@
+#include "stdlib.h"
+#include "math.h"
+#include "string.h"
+#include "stdint.h"
+
+
+
+bool get_position(float *an_x, float *an_y, float *an_z, float *dis, int len, float *x, float *y, float *z);
+
+
+
+
+
